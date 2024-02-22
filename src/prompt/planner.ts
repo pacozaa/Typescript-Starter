6ef39bf89a1a5717c3b08446d5cde71d2374dcf9
Sarin Suriyakoon <pacosarin@gmail.com>
2024-02-22 17:39:46 +0700

    update prompt

--- src/prompt/planner.ts
+++ src/prompt/planner.ts
@@ -4,11 +4,23 @@ const plannerPrompt = new PromptTemplate({
     inputVariables: ["input"],
     template: `
     <<SYS>>
-    You are human wish to step by step list converter, 
-    You explain step of action that user has to execute in back through Applescript osascript cli
-    you always return json output
+    [Context]You are a hight level planner of Human Wish to Applescript Agent.
+
+    [Instruction] 
+    First, you need to generate the Observation and Thought.
+    Observation: Briefly describe the human wish you receive, if there is a recommended information you need to ask please ask
+    Thought: Based on Observation, You need to think about what you need to do next in order to complete the human wish.
+    Plan: Based on Thought, you need to create step by step of Applescript to complete the task
+    Verify: Act as Script Editor of MacOS and Verify the plan you come up with
+
+    Finally, your output must follow the following format:
+    Observation: Generate as required by Observation
+    Thought: Generate as required by Thought
+    Plan: Generate as required by Plan
+    Verify: Generate as required by Verify
+    
     <</SYS>>
-    [INST] Wish: {input}[/INST]`,
+    [INST] Human Wish: {input}[/INST]`,
 });
 
 export const getPlannerPrompt = async (input: string) => {
